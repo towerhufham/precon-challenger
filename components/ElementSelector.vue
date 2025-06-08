6cf98a3fb49a101e45eb232d99fa5cb97098a44c
towerhufham <32599109+towerhufham@users.noreply.github.com>
2025-06-08 19:36:42 -0400

    Better element icon styling

--- components/ElementSelector.vue
+++ components/ElementSelector.vue
@@ -4,18 +4,17 @@
   <!-- though there might be a cooler way of doing that with TS -->
   <div v-if="criteria.type === 'Element'">
     <p>Select element:</p>
-    <div class="flex gap-4">
+    <div class="flex gap-4 items-center">
       <ElementalIcon 
         v-for="el of ALL_ELEMENTS"
         :elemental="el" 
         size="3em" 
         :class="{
-          'text-slate-300': !shownElements.includes(el), 
-          'text-white': selected === el,
-          'hover:text-slate-600': selected !== el
+          'grayscale': !shownElements.includes(el), 
+          'border-4 border-white box-border': selected === el,
         }"
-        @click="selected = el"/>
-        <p v-if="selected" @click="selected = null" class="hover:text-slate-600 cursor-pointer">(Deselect)</p>
+        class="cursor-pointer"
+        @click="selected === el ? selected = null : selected = el"/>
     </div>
   </div>
 </template>
